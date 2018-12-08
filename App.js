3a1565294c5a0e80dbed543cffd42f932255f5ba
Tsyirvo <gabriel.hofman.00@gmail.com>
2018-12-08 18:58:56 +0100

    Use react-native-screens to optimise navigation

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useScreens } from 'react-native-screens';
 
 import AppContainer from 'routes/routes';
+
+useScreens();
 
 const Root = () => <AppContainer />;
 
