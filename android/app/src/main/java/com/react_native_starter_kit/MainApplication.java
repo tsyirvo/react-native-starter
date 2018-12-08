0d1644967eca41a3cd537c3fae00b83e03a37707
Tsyirvo <gabriel.hofman.00@gmail.com>
2018-12-08 19:21:41 +0100

    Add Lottie

--- android/app/src/main/java/com/react_native_starter_kit/MainApplication.java
+++ android/app/src/main/java/com/react_native_starter_kit/MainApplication.java
@@ -3,6 +3,7 @@ package com.react_native_starter_kit;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.lottie.LottiePackage;
 import com.swmansion.rnscreens.RNScreensPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LottiePackage(),
             new RNScreensPackage(),
             new RNGestureHandlerPackage()
       );
