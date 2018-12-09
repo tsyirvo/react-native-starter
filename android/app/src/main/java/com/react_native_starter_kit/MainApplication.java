add33ec95a3fc6922930bfc05c2747b39dc903ba
Tsyirvo <gabriel.hofman.00@gmail.com>
2018-12-09 16:57:36 +0100

    Configure RN config for Android

--- android/app/src/main/java/com/react_native_starter_kit/MainApplication.java
+++ android/app/src/main/java/com/react_native_starter_kit/MainApplication.java
@@ -3,6 +3,7 @@ package com.react_native_starter_kit;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.airbnb.android.react.lottie.LottiePackage;
 import com.swmansion.rnscreens.RNScreensPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeConfigPackage(),
             new LottiePackage(),
             new RNScreensPackage(),
             new RNGestureHandlerPackage()
--- src/components/home/components/Home.js
+++ src/components/home/components/Home.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Config from 'react-native-config';
 
 import Button from 'shared/Button';
 
-const Home = ({ navigation }) => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Home Component</Text>
+const Home = ({ navigation }) => {
+  console.log('config', Config.NODE_ENV);
 
-    <Button onPress={() => navigation.navigate('Details')}>
-      <Text>Go to details</Text>
-    </Button>
-  </View>
-);
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home Component</Text>
+
+      <Button onPress={() => navigation.navigate('Details')}>
+        <Text>Go to details</Text>
+      </Button>
+    </View>
+  );
+};
 
 export default Home;
