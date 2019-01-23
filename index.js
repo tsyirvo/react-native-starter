0cfd313b90870b44955182ba2f3ce9c3916b176d
Tsyirvo <gabriel.hofman.00@gmail.com>
2019-01-23 22:58:27 +0100

    Fix bad env property check && remove launching the storybook by default

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { AppRegistry } from 'react-native';
 
-// import Root from './App';
-import Storybook from './storybook';
+import Root from './App';
+// import Storybook from './storybook';
 
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => Storybook);
+AppRegistry.registerComponent(appName, () => Root);
