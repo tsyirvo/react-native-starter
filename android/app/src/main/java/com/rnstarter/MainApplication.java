a2b678e1a7372a2004890fcb643fb514553202fd
Gabriel Hofman <gabriel.hofman.00@gmail.com>
2020-02-01 23:15:29 +0100

    feat: wip of adding a link to the storybook from the dev menu

--- android/app/src/main/java/com/rnstarter/MainApplication.java
+++ android/app/src/main/java/com/rnstarter/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.zoontek.rndevmenu.RNDevMenuPackage;
 import org.reactnative.maskedview.RNCMaskedViewPackage;
 import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
