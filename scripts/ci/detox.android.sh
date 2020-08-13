0be7efe9bf6713db28e7e80e0537db9dd97faeb6
Gabriel Hofman <gabriel.hofman.00@gmail.com>
2020-08-13 19:34:49 +0200

    feat: fix ios build cache and prettier build output

--- scripts/ci/detox.android.sh
+++ scripts/ci/detox.android.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
-detox build -c android.emu.ci
-detox test -c android.emu.ci -n test -H --cleanup
+npx detox build -c android.emu.ci
+npx detox test -c android.emu.ci -n test -H --cleanup
--- scripts/ci/detox.ios.sh
+++ scripts/ci/detox.ios.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
-set -o pipefail && xcodebuild -workspace ios/rnStarter.xcworkspace -scheme rnStarter -configuration Release -sdk iphonesimulator -derivedDataPath ios/build | xcpretty
-detox test --configuration ios.sim.release --cleanup
+npx detox clean-framework-cache && npx detox build-framework-cache
+set -o pipefail && xcodebuild -workspace ios/rnStarter.xcworkspace -scheme rnStarter -configuration Release -sdk iphonesimulator -derivedDataPath ios/build | xcpretty -f `xcpretty-travis-formatter`
+npx detox test --configuration ios.sim.release --cleanup
