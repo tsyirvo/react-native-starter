3be8e859b007f65d53cd9faa2340d91d0dead849
Gabriel Hofman <gabriel.hofman.00@gmail.com>
2020-08-15 16:37:42 +0200

    ci: try something new

--- scripts/ci/emulator.android.sh
+++ scripts/ci/emulator.android.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-echo no | avdmanager create avd --force -n test  -k "system-images;android-$API;$EMU_FLAVOR;$ABI" -c 10M
+echo no | avdmanager create avd --force -n test -k "system-images;android-$API;$EMU_FLAVOR;$ABI" -c 10M
 emulator -verbose -avd test -no-accel -no-snapshot -no-window -noaudio -camera-back none -camera-front none -selinux permissive -qemu -m 2048 &
 android-wait-for-emulator
 adb shell input keyevent 82 &
