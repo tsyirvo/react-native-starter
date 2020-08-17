7d592279b765cf7c63595598c77a067688386380
Gabriel Hofman <gabriel.hofman.00@gmail.com>
2020-08-17 21:15:21 +0200

    feat: better theme file

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,10 +1,35 @@
+const brandColors = {};
+
 export default {
   colors: {
-    black: '#000e1a',
+    ...brandColors,
+    black: '#000',
     white: '#fff',
-    blue: '#007ce0',
-    navy: '#004175',
+    blue: '#457b9d',
+    green: '#2a9d8f',
+    yellow: '#e9c46a',
+    red: '#ef233c',
   },
-  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
-  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 72],
+  space: {
+    small: 8,
+    medium: 16,
+    large: 24,
+    xLarge: 32,
+  },
+  fonts: {
+    ios: 'San Francisco',
+    android: 'Roboto',
+  },
+  fontSizes: {
+    small: 12,
+    medium: 14,
+    regular: 16,
+    large: 24,
+    xLarge: 32,
+  },
+  radii: {
+    small: 4,
+    regular: 8,
+    medium: 16,
+  },
 };
