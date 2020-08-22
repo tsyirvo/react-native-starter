2213ad15c218edbb76dbfe4069cabc73de05e7a2
Gabriel Hofman <gabriel.hofman.00@gmail.com>
2020-08-22 18:41:38 +0200

    chore: jest config cleanup

---
+++ src/tests/__mocks__/firebase.ts
@@ -0,0 +1,8 @@
+jest.mock('@react-native-firebase/analytics', () => {
+  return () => ({
+    logEvent: jest.fn(),
+    setUserProperties: jest.fn(),
+    setUserId: jest.fn(),
+    setCurrentScreen: jest.fn(),
+  });
+});
