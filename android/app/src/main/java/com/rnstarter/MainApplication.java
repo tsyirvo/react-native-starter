014bcc8059061eea6975e465c44cdd91a821cb11
Gabriel Hofman <gabriel.hofman.00@gmail.com>
2020-08-31 12:49:28 +0200

    feat: configure codepush for android

--- android/app/src/main/java/com/rnstarter/MainApplication.java
+++ android/app/src/main/java/com/rnstarter/MainApplication.java
@@ -11,10 +11,17 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+import com.microsoft.codepush.react.CodePush;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
       new ReactNativeHost(this) {
+        @Override
+        protected String getJSBundleFile() {
+          return CodePush.getJSBundleFile();
+        }
+
         @Override
         public boolean getUseDeveloperSupport() {
           return BuildConfig.DEBUG;
