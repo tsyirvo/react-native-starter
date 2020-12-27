c557e1806e1444b5b604ded897f388dc768bab03
Gabriel Hofman <gabriel.hofman.00@gmail.com>
2020-12-27 11:48:30 +0100

    refactor: update some logic

--- android/app/src/main/java/ninja/gab/rnStarter/MainApplication.java
+++ android/app/src/main/java/ninja/gab/rnStarter/MainApplication.java
@@ -1,4 +1,4 @@
-package com.rnstarter;
+package ninja.gab.rnStarter;
 
 import android.content.Context;
 import com.facebook.react.PackageList;
@@ -69,7 +69,7 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.rnstarter.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("ninja.gab.rnStarter.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
