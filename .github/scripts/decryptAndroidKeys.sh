48921e95eee81475221f5b5ded9834956105fc16
Gabriel Hofman <gabriel.hofman.00@gmail.com>
2021-12-16 21:21:46 +0100

    feat: create a script to get all Android secrets

---
+++ .github/scripts/decryptAndroidKeys.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+# Keystore file
+echo "$RELEASE_KEYSTORE" > release.keystore.asc 
+gpg -d --passphrase="$GPG_PASSPHRASE" --batch release.keystore.asc > android/app/release.keystore 
+
+echo "$GRADDLE_PROPERTIES" > gradle.properties.asc 
+gpg -d --passphrase="$GPG_PASSPHRASE" --batch gradle.properties.asc > android/gradle.properties 
+
+# Google JSON key
+echo "$GOOGLE_CREDENTIAL_KEY" > google-credential-key.asc 
+gpg -d --passphrase="$GPG_PASSPHRASE" --batch google-credential-key.asc > android/google-credential-key.json 
+
+# Fastlane .env
+echo "${{secrets.FASTLANE_ENV_ANDROID}}" > .env.fastlane.android.asc
+gpg -d --passphrase "${{secrets.GPG_PASSPHRASE}}" --batch .env.fastlane.android.asc > android/fastlane/.env
+
+# App .env
+echo "${{secrets.STAGING_ENV}}" > .env.staging.asc
+gpg -d --passphrase "${{secrets.GPG_PASSPHRASE}}" --batch .env.staging.asc > .env.staging
+
+# Delete base files
+rm release.keystore.asc gradle.properties.asc google-credential-key.asc .env.fastlane.android.asc .env.staging.asc
