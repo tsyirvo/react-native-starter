6160deebc95568389619d8b51f222a96b8fba7aa
Gabriel Hofman <gabriel.hofman.00@gmail.com>
2021-12-16 21:49:20 +0100

    fix: bad gpg env fix

--- .github/scripts/decryptAndroidKeys.sh
+++ .github/scripts/decryptAndroidKeys.sh
@@ -12,12 +12,12 @@ echo "$GOOGLE_CREDENTIAL_KEY" > google-credential-key.asc
 gpg -d --passphrase="$GPG_PASSPHRASE" --batch google-credential-key.asc > android/google-credential-key.json 
 
 # Fastlane .env
-echo "${{secrets.FASTLANE_ENV_ANDROID}}" > .env.fastlane.android.asc
-gpg -d --passphrase "${{secrets.GPG_PASSPHRASE}}" --batch .env.fastlane.android.asc > android/fastlane/.env
+echo "$FASTLANE_ENV_ANDROID" > .env.fastlane.android.asc
+gpg -d --passphrase="$GPG_PASSPHRASE" --batch .env.fastlane.android.asc > android/fastlane/.env
 
 # App .env
-echo "${{secrets.STAGING_ENV}}" > .env.staging.asc
-gpg -d --passphrase "${{secrets.GPG_PASSPHRASE}}" --batch .env.staging.asc > .env.staging
+echo "$STAGING_ENV" > .env.staging.asc
+gpg -d --passphrase="$GPG_PASSPHRASE" --batch .env.staging.asc > .env.staging
 
 # Delete base files
 rm release.keystore.asc gradle.properties.asc google-credential-key.asc .env.fastlane.android.asc .env.staging.asc
