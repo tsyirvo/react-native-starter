ec54e3daa68f2f6f1f55f07d2d0da14adf98cc2c
Gabriel Hofman <gabriel.hofman.00@gmail.com>
2021-12-30 19:44:06 +0100

    feat: create an analytics abstraction

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 
+import Analytics from '$core/analytics';
 import { ErrorMonitoring } from '$core/monitoring';
 
 import { name as appName } from './app.json';
 import Root from './src/App.tsx';
 
 ErrorMonitoring.init();
+Analytics.init();
 
 AppRegistry.registerComponent(appName, () => Root);
--- src/core/constants/index.ts
+++ src/core/constants/index.ts
@@ -1,3 +1,4 @@
 export { default as config } from './config';
 export * from './dimensions';
+export * from './platform';
 export { default as storageKeys } from './storage';
