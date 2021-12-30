92793fc37755360a78156b2affe1bb0fe9f19c8a
Gabriel Hofman <gabriel.hofman.00@gmail.com>
2021-12-30 18:00:21 +0100

    feat: add a logger class to handle errors (#267)

---
+++ src/core/logger/logger.types.ts
@@ -0,0 +1,28 @@
+/* ***** *****  Toast Message  ***** ***** */
+
+type EmptyMessage = {};
+export type ValidMessage = { title: string; message: string };
+
+export type UserMessageType = EmptyMessage | ValidMessage;
+
+/* ***** *****  Network  ***** ***** */
+
+export type NetworkErrorType = {
+  description?: string;
+  requestData: {
+    request: string;
+    method?: string;
+    statusCode?: number;
+    reason?: string;
+  };
+  userMessage: UserMessageType;
+};
+
+/* ***** *****  Error  ***** ***** */
+
+export type ErrorType = {
+  error: unknown;
+  type: string;
+  message: string;
+  userMessage: UserMessageType;
+};
