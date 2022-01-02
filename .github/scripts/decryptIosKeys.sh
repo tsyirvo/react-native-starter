16d58e30e691b1c1a083c7dacbe24f0f3f7db453
Gabriel Hofman <gabriel.hofman.00@gmail.com>
2022-01-02 15:59:59 +0100

    fix: add missing files for the CI build (#276)
    * chore: cleanup iOS firebase files
    
    * fix: add missing files for the CI build

--- .github/scripts/decryptIosKeys.sh
+++ .github/scripts/decryptIosKeys.sh
@@ -12,5 +12,13 @@ gpg -d --passphrase="$GPG_PASSPHRASE" --batch .env.fastlane.ios.asc > ios/fastla
 echo "$STAGING_ENV" > .env.staging.asc
 gpg -d --passphrase="$GPG_PASSPHRASE" --batch .env.staging.asc > .env.staging
 
+# Sentry config
+echo "$SENTRY_CONFIG" > sentry.properties.asc
+gpg -d --passphrase="$GPG_PASSPHRASE" --batch sentry.properties.asc > ios/sentry.properties
+
+# Firebase Staging
+echo "$FIREBASE_STAGING_IOS" > GoogleService-Info.plist.asc
+gpg -d --passphrase="$GPG_PASSPHRASE" --batch GoogleService-Info.plist.asc > ios/Firebase/Staging/GoogleService-Info.plist
+
 # Delete base files
-rm AuthKey_7GF3V4HFK9.p8.asc .env.fastlane.ios.asc .env.staging.asc
+rm AuthKey_7GF3V4HFK9.p8.asc .env.fastlane.ios.asc .env.staging.asc sentry.properties.asc GoogleService-Info.plist.asc
