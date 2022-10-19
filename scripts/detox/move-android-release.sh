8600c631a31d27bf47f18ae0633b920f8d86497f
tsyirvo <gabriel.hofman.00@gmail.com>
2022-10-19 17:11:42 +0200

    chore: final touches to have everything working together

--- scripts/detox/move-android-release.sh
+++ scripts/detox/move-android-release.sh
@@ -3,8 +3,8 @@ set -e
 
 tar -xf e2e/builds/android-release.tar.gz -C e2e/builds
 
-mv "$PWD/release/app-release.apk" "$PWD/e2e/builds/"
-mv "$PWD/androidTest/release/app-release-androidTest.apk" "$PWD/e2e/builds/"
+mv "$PWD/e2e/builds/release/app-release.apk" "$PWD/e2e/builds/"
+mv "$PWD/e2e/builds/androidTest/release/app-release-androidTest.apk" "$PWD/e2e/builds/"
 
 rm -rf "$PWD/e2e/builds/release"
 rm -rf "$PWD/e2e/builds/androidTest"
