b93748c6b0fa5f027fbc30ab067d745c541e045d
tsyirvo <1540619+tsyirvo@users.noreply.github.com>
2025-03-08 15:18:49 +0100

    refactor: rework the screen component

--- src/shared/uiKit/Screen.tsx
+++ src/shared/uiKit/Screen.tsx
@@ -1,8 +1,7 @@
 import type { ReactNode } from 'react';
-import { ScrollView } from 'react-native';
 import type { Edge } from 'react-native-safe-area-context';
 
-import type { Colors, Spacing } from '$core/theme';
+import { makeAppStyles, type Colors, type Spacing } from '$core/theme';
 
 import { Box } from './primitives';
 import { SafeView } from './SafeView';
@@ -10,25 +9,33 @@ import { SafeView } from './SafeView';
 type ScreenProps = {
   edges?: Edge[];
   children: ReactNode;
-  isScrollable?: boolean;
   bg?: Colors;
-  p?: Spacing;
+  px?: Spacing;
+  py?: Spacing;
   testID?: string;
 };
 
 export const Screen = ({
   children,
   edges = [],
-  isScrollable = true,
   bg = 'bg',
-  p = 'spacing_16',
+  px = 'zero',
+  py = 'zero',
   testID,
 }: ScreenProps) => {
+  const styles = useStyles();
+
   return (
-    <SafeView edges={edges}>
-      <Box bg={bg} flex={1} p={p} testID={testID}>
-        {isScrollable ? <ScrollView>{children}</ScrollView> : children}
-      </Box>
-    </SafeView>
+    <Box bg={bg} flex={1} py={py} px={px} testID={testID}>
+      <SafeView edges={edges} style={styles.wrapper}>
+        {children}
+      </SafeView>
+    </Box>
   );
 };
+
+const useStyles = makeAppStyles(() => ({
+  wrapper: {
+    flex: 1,
+  },
+}));
