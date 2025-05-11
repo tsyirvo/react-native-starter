d860c3395f1ef0e04b1097feb4675e4ea763de3d
Gabriel Hofman <1540619+tsyirvo@users.noreply.github.com>
2025-05-11 14:42:31 +0200

    fix: handle wrong path on feature flag logic (#417)

--- src/infra/featureFlags/hooks/useGetRemoteConfig.ts
+++ src/infra/featureFlags/hooks/useGetRemoteConfig.ts
@@ -6,11 +6,12 @@ import type { AvailableRemoteConfig } from '../featureFlags.types';
 export function useGetRemoteConfig(flagKey: AvailableRemoteConfig) {
   const flag = useFeatureFlagWithPayload(flagKey);
 
-  if (!flag[FLAG_VALUE_INDEX]) {
-    return defaultRemoteConfig[flagKey];
+  if (flag[FLAG_VALUE_INDEX] !== undefined) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return flag[FLAG_VALUE_INDEX];
   }
 
-  return flag;
+  return defaultRemoteConfig[flagKey];
 }
 
 const FLAG_VALUE_INDEX = 1;
