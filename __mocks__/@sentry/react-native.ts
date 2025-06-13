8321587809a8d7285e4671bd1d637b0acfe6c73f
tsyirvo <gabriel.hofman.00@gmail.com>
2025-06-13 11:18:35 +0200

    test: add sentry mock to fix open handles

---
+++ __mocks__/@sentry/react-native.ts
@@ -0,0 +1,14 @@
+module.exports = {
+  reactNavigationIntegration: jest.fn(),
+  init: jest.fn(),
+  setTag: jest.fn(),
+  setExtra: jest.fn(),
+  setUser: jest.fn(),
+  setContext: jest.fn(),
+  captureException: jest.fn(),
+  captureEvent: jest.fn(),
+  addBreadcrumb: jest.fn(),
+  withScope: jest.fn(),
+  startSpan: jest.fn(),
+  startInactiveSpan: jest.fn(),
+};
